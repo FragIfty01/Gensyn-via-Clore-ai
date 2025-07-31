e5567fdf583c128d2db93e6154afcccf5df40914
FragIfty01 <146715637+FragIfty01@users.noreply.github.com>
2025-07-31 16:17:36 +0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,32 +4,32 @@ set -e
 
 echo "🔄 Updating system and installing dependencies..."
 
-sudo apt-get update && sudo apt-get upgrade -y
+apt-get update && apt-get upgrade -y
 
-sudo apt-get install -y nvidia-cuda-toolkit
-sudo apt-get install -y \
+apt-get install -y nvidia-cuda-toolkit
+apt-get install -y \
   screen curl iptables build-essential git wget lz4 jq make gcc nano \
   automake autoconf tmux htop nvme-cli libgbm1 pkg-config libssl-dev \
   libleveldb-dev tar clang bsdmainutils ncdu unzip
 
 # Node.js 22.x & yarn
-curl -fsSL https://deb.nodesource.com/setup_22.x | sudo -E bash -
-sudo apt install -y nodejs
+curl -fsSL https://deb.nodesource.com/setup_22.x | bash -
+apt install -y nodejs
 node -v
 
-sudo npm install -g yarn
+npm install -g yarn
 yarn -v
 
-# Install yarn (alternative method, in case the above fails)
+# Install yarn (alternative method)
 curl -o- -L https://yarnpkg.com/install.sh | bash
 export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
 
 # Python dev
-sudo apt install -y python3.12-venv python3.12-dev
+apt install -y python3.12-venv python3.12-dev
 
 # Check for swarm.pem
 if [ ! -f "swarm.pem" ]; then
-  echo -e "\n\033[1;33m⚠️  Please copy your swarm.pem file into the rl-swarm directory before continuing! \033[0m"
+  echo -e "\n\033[1;33m⚠️  Please copy your swarm.pem file into the Gensyn-via-Clore-ai directory before continuing! \033[0m"
   echo "After copying swarm.pem here, rerun this script."
   exit 1
 fi
